464adde208e76b55bfb616b9c831c1b377e083bb
whazys <santos_rafa1973@hotmail.com>
2022-04-22 12:21:51 -0300

    Att.

---
+++ Módulo IV/States/ Effects - React/hooks/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import Counter from '../Counter/Counter.js';
+import SmartCounter from "../Smartcounter/Smartcounter";
+import Ifoodcounter from "../Ifoodcounter/Ifoodcounter";
+
+function App() {
+  return (
+      <>
+        <h1>Ola Rafaaa</h1>
+        <Ifoodcounter/>
+      </>
+  );
+}
+
+export default App;
